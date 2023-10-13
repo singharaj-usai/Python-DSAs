c21c139a4a498cfafa777050e6beff22478b135f
singharaj usai <djjd22938@gmail.com>
2023-10-13 04:41:25 -0400

    linearSearch solved

--- linearSearch.py
+++ linearSearch.py
@@ -1,4 +1,13 @@
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7]g
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7]
+
 
 # TODO: Complete the linearSearch function below.
-def  linearSearch = (array, element):
+def linearSearch(array, element):
+    for index in range(len(array)):
+        if array[index] == element:
+            return index
+
+    return -1
+
+
+print(linearSearch(arr, 77))
