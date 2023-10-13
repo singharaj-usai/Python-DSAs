cafb850e12ddf26ab2ba6d95d20b6c28ab64d502
singharaj usai <djjd22938@gmail.com>
2023-10-13 05:23:05 -0400

    evenindexmult solved

--- evenIndexMultiplier.py
+++ evenIndexMultiplier.py
@@ -1,2 +1,15 @@
 arrayIndex = [1, 52, 35, 6, 72, 7, 3, 19, 32, 54, 78, 95, 97]
 
+
+def evenIndexMultiplier(arr):
+    result = []
+    for index, number in enumerate(arr):
+        if index % 2 == 0:
+            result.append(number * 10)
+        else:
+            result.append(number)
+    return result
+
+
+evenIndexes = evenIndexMultiplier(arrayIndex)
+print(evenIndexes)
