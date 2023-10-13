73d6b11cfe9451b92ceb93d67c513efcb0606798
singharaj usai <djjd22938@gmail.com>
2023-10-13 05:43:07 -0400

    queues solved

--- queues.py
+++ queues.py
@@ -2,3 +2,19 @@ from collections import deque
 
 # Complete this Queue function that adds and removes queues
 class Queue:
+    def __int__(self, container=None):
+        if container is None:
+            container = deque()
+        self.container = container
+
+    def add(self, element):
+        self.container.append(element)
+
+    def remove(self):
+        if not self.isEmpty():
+            return self.container.popleft()
+        else:
+            raise IndexError("Queue empty")
+
+    def isEmpty(self):
+        return len(self.container) == 0
